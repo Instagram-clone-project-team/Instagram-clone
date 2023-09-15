3a9c7b34fb431b559188cca9cc40127075100a45
Dongyeop Lee <dlduq29@gmail.com>
2023-09-15 13:23:24 +0900

    feat - ErrorCode,ResultCode 추가

--- src/main/java/com/project/Instagram/global/error/ErrorCode.java
+++ src/main/java/com/project/Instagram/global/error/ErrorCode.java
@@ -8,12 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     PASSWORD_MISMATCH(401,"계정 비밀번호가 일치하지 않습니다."),
-    PASSWORD_SAME(400,"기존 비밀번호와 같습니다.");
-
-
-
-
-
+    PASSWORD_SAME(400,"기존 비밀번호와 같습니다."),
+    MEMBER_NOT_FOUND(400,"존재 하지 않는 유저입니다."),
+    PASSWORD_RESET_FAIL(400, "잘못되거나 만료된 코드입니다."),
+    USERNAME_ALREADY_EXIST(400,"해당 유저네임은 이미 존재합니다.");
 
 
     private final int status;
--- src/main/java/com/project/Instagram/global/response/ResultCode.java
+++ src/main/java/com/project/Instagram/global/response/ResultCode.java
@@ -11,8 +11,8 @@ public enum ResultCode {
     UPDATE_PASSWORD_SUCCESS(200,"비밀번호 변경이 완료되었습니다."),
     REGISTER_SUCCESS(200, "회원가입에 성공하였습니다."),
     DELETE_SUCCESS(200,"회원 탈퇴 완료되었습니다."),
-    RESET_PASSWORD_SUCCESS(200, "비밀번호 리셋 완료되었습니다.");
-
+    RESET_PASSWORD_SUCCESS(200, "비밀번호 리셋 완료되었습니다."),
+    UPDATE_ACCOUNT_SUCCESS(200,"계정 정보 변경이 완료되었습니다.");
 
 
     //Post
