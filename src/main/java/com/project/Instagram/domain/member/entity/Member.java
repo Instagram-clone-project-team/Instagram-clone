d414723dc1422966ba7c05ff1adb23777cf6bb22
Heoyungyoung <gjdbs2597@naver.com>
2023-09-20 15:30:05 +0900

    회원 수정 병합

--- src/main/java/com/project/Instagram/domain/member/entity/Member.java
+++ src/main/java/com/project/Instagram/domain/member/entity/Member.java
@@ -69,5 +69,11 @@ public class Member extends BaseTimeEntity {
         this.name = name;
     }
 
-
+    public void updateUsername(String username){this.username = username;}
+    public void updateName(String name){this.name = name;}
+    public void updateLink(String link){this.link = link;}
+    public void updateIntroduce(String introduce){this.introduce = introduce;}
+    public void updateEmail(String email){this.email = email;}
+    public void updatePhone(String phone){this.phone = phone;}
+    public void updateGender(Gender gender){this.gender = gender;}
 }
