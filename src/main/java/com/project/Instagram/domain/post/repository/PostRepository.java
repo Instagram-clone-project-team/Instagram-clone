8e90caca22387b12a92adb88210fcd1bd3a3db01
Heoyungyoung <gjdbs2597@naver.com>
2023-09-25 17:21:06 +0900

    refactor-레파지토리 @Param 적용

--- src/main/java/com/project/Instagram/domain/post/repository/PostRepository.java
+++ src/main/java/com/project/Instagram/domain/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.project.Instagram.domain.post.repository;
 
 import com.project.Instagram.domain.post.entity.Post;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p join fetch p.member where p.id = :postId")
-    Optional<Post> findWithMemberById(Long postId);
+    Optional<Post> findWithMemberById(@Param("postId") Long postId);
 }
