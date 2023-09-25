a83974f4f0f98e3f2ca8e99bdcd821439b46caab
Heoyungyoung <gjdbs2597@naver.com>
2023-09-25 17:15:19 +0900

    feat-게시판 삭제

--- src/main/java/com/project/Instagram/domain/post/controller/PostController.java
+++ src/main/java/com/project/Instagram/domain/post/controller/PostController.java
@@ -1,6 +1,18 @@
 package com.project.Instagram.domain.post.controller;
 
+import com.project.Instagram.domain.post.service.PostService;
+import com.project.Instagram.global.response.ResultCode;
+import com.project.Instagram.global.response.ResultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
 
     // 등록
 
@@ -9,4 +21,9 @@ public class PostController {
     // 수정
 
     // 삭제
+    @DeleteMapping("/delete/{post_id}")
+    public ResponseEntity<ResultResponse> deletePost(@PathVariable("post_id") Long postId) {
+        postService.delete(postId);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.DELETE_POST_SUCCESS));
+    }
 }
--- src/main/java/com/project/Instagram/global/response/ResultCode.java
+++ src/main/java/com/project/Instagram/global/response/ResultCode.java
@@ -15,11 +15,10 @@ public enum ResultCode {
     UPDATE_ACCOUNT_SUCCESS(200,"계정 정보 변경이 완료되었습니다."),
     SEND_EMAIL_SUCCESS(200,  "인증코드 이메일을 전송하였습니다."),
     EMAIL_VERIFICATION_FAIL(400, "이메일 인증에 실패했습니다."),
-    LOGOUT_SUCCESS(200, "로그아웃에 성공하였습니다.");
-
-
+    LOGOUT_SUCCESS(200, "로그아웃에 성공하였습니다."),
 
     //Post
+    DELETE_POST_SUCCESS(200, "게시물 삭제에 성공하였습니다.");
 
 
 
