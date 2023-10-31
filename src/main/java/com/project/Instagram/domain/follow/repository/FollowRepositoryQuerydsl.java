183ff2285008d1055d306de7558dd08d0a8f1e23
Heoyungyoung <gjdbs2597@naver.com>
2023-10-31 16:32:43 +0900

    reactor - 주석 삭제

--- src/main/java/com/project/Instagram/domain/follow/repository/FollowRepositoryQuerydsl.java
+++ src/main/java/com/project/Instagram/domain/follow/repository/FollowRepositoryQuerydsl.java
@@ -1,7 +1,6 @@
 package com.project.Instagram.domain.follow.repository;
 
 import com.project.Instagram.domain.follow.dto.FollowerDto;
-import com.project.Instagram.domain.follow.entity.Follow;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -12,6 +11,5 @@ public interface FollowRepositoryQuerydsl {
 
     Page<FollowerDto> findFollowers(Long loginId, Long memberId, Pageable pageable);
 
-//    List<Follow> findFollows(Long memberId, List<Long> agentIds);
 }
 
